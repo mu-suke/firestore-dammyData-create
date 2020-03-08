7de8d76898244c8331ef5f18f1796f9be6fb3f2a
mu-suke <chelsea_08ym@yahoo.co.jp>
2020-03-08 15:55:54 +0900

    npmスクリプトの追加とlint対策

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
-const admin = require("firebase-admin");
+const admin = require('firebase-admin');
 
 main();
 
+/**
+  * @typedef {object} Weather
+  * @property {string} current The current weather.
+  */
 function main() {
-  console.log("hogehoge");
+  console.log('hogehoge');
 }
