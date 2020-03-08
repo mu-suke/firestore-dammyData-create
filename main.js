8e6a030304ab51add53a15bfac504dcbbd38ea8f
mu-suke <chelsea_08ym@yahoo.co.jp>
2020-03-09 01:43:11 +0900

    createdAtの対応完了

--- main.js
+++ main.js
@@ -1,11 +1,43 @@
 const admin = require('firebase-admin');
+const serviceAccount = require('./firebase/serviceAccountKey.json');
 
-main();
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://sales-management-2041e.firebaseio.com',
+});
 
-/**
-  * @typedef {object} Weather
-  * @property {string} current The current weather.
-  */
-function main() {
-  console.log('hogehoge');
+const db = admin.firestore();
+db.collection('stores/v1/store1').get()
+    .then((snapshot) => {
+      snapshot.forEach((doc) => {
+        console.log(doc.id, '=>', doc.data());
+      });
+    })
+    .catch((err) => {
+      console.log('Error getting documents', err);
+    });
+
+let store = db.collection('stores').doc();
+
+const QUANTITY_CONST = {
+  MIN: 5,
+  MAX: 10,
+};
+
+const TIME_CONST = {
+  MIN: 10,
+  MAX: 17,
 }
+
+let setSf = store.set({
+  quantity: Math.floor(Math.random() * (QUANTITY_CONST.MAX + 1 - QUANTITY_CONST.MIN)) + QUANTITY_CONST.MIN,
+  createdAt: admin.firestore.Timestamp.fromDate(new Date(`2018/11/10 ${Math.floor(Math.random() * (TIME_CONST.MAX + 1 - TIME_CONST.MIN) + TIME_CONST.MIN)}:00:00`)),
+});
+
+const storeList = [
+  'hogehoge',
+  'fugafuga',
+  'piyopiyo',
+];
+
+
