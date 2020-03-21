b4c4cda5ba12860c8eef659503d81a1be7109ab1
mu-suke <chelsea_08ym@yahoo.co.jp>
2020-03-21 16:31:53 +0900

    async,await要らなかった

--- main.js
+++ main.js
@@ -72,9 +72,10 @@ storeAndQuantityDataList.forEach((storeData) => {
                 return;
               }
               collections.forEach(collection => {
-                console.log('Found subcollection with id:', collection.id);
-                // TODO: async awaitを実装する
-
+                db.collection(`stores/${doc.id}/${collection.id}`).add({
+                  quantity: Math.floor(Math.random() * (QUANTITY_CONST.MAX + 1 - QUANTITY_CONST.MIN)) + QUANTITY_CONST.MIN,
+                  createdAt: admin.firestore.Timestamp.fromDate(new Date(`2018/11/10 ${Math.floor(Math.random() * (TIME_CONST.MAX + 1 - TIME_CONST.MIN) + TIME_CONST.MIN)}:00:00`)),
+                });
                 sample(doc, collection).then(result => {
                   console.log(result);
                 });
@@ -105,22 +106,3 @@ data && Object.keys(data).forEach(key => {
       });
 });
 
-function sampleResolve(doc, coll) {
-  return new Promise(resolve => {
-    db.collection(`stores/${doc.id}/${coll.id}`).add({
-      quantity: Math.floor(Math.random() * (QUANTITY_CONST.MAX + 1 - QUANTITY_CONST.MIN)) + QUANTITY_CONST.MIN,
-      createdAt: admin.firestore.Timestamp.fromDate(new Date(`2018/11/10 ${Math.floor(Math.random() * (TIME_CONST.MAX + 1 - TIME_CONST.MIN) + TIME_CONST.MIN)}:00:00`)),
-    });
-    resolve("success!");
-  })
-}
-
-/**
-* sampleResolve()をawaitしているため、Promiseの結果が返されるまで処理が一時停止される
-* 今回の場合、2秒後にresolve(10)が返ってきてその後の処理（return result + 5;）が再開される
-* resultにはresolveされた10が格納されているため、result + 5 = 15がreturnされる
-*/
-async function sample(doc, coll) {
-  const result = await sampleResolve(doc, coll);
-  return result;
-}
